c8cb0306abfd9d5b3db4df49c8592f65300c5eae
kehantan <kehantan321@gmail.com>
2022-06-05 14:22:48 +0800

    Script added.

---
+++ post_dock_prep.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+echo "exhaustiveness?"
+read ex
+echo "docking type? (blind/bsite)"
+read type
+echo "scoring? (vina/vinardo)"
+read score
+
+for i in {1..5}
+do
+	mkdir v"$i"
+	cp out_v"$i".pdbqt v"$i"
+done
+
+for i in {1..5}
+do 
+	cd v"$i"
+	vina_split --input out_v"$i".pdbqt
+	cd ../
+done
+
+for i in {1..5}
+do 
+	cd v"$i"
+	for j in {1..9}
+	do 
+		obabel -i pdbqt out_v"$i"_ligand_"$j".pdbqt -o pdb -O "$score"_e"$ex"_"$type"_v"$i"_out_"$j".pdb
+	done
+	cd ../
+done
